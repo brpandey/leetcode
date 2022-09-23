2c7ffe97c67662fb11e6eaf98da8d0ec6643b967
Bibek Pandey <bibekp@gmail.com>
2022-09-23 12:59:57 -0700

    Added second solution for o(1) space for 42 trapping rain water

--- rust/src/p0042_trapping_rain_water.rs
+++ rust/src/p0042_trapping_rain_water.rs
@@ -40,32 +40,59 @@ pub struct Solution {}
 
 impl Solution {
     pub fn trap(height: Vec<i32>) -> i32 {
-            
-        let mut left = vec![0; height.len()];
-        let mut right = vec![0; height.len()];
-        let mut max = 0;
+        let len = height.len();
+        let mut left_wall = vec![0; len];
+        let mut right_wall = vec![0; len];
+        let (mut max, mut water) = (0, 0);
 
-        for i in 0..height.len()-1 {
+        for i in 0..len-1 {
             max = std::cmp::max(max, height[i]);
-            left[i+1] = max;
+            left_wall[i+1] = max;
         }
 
         max = 0;
 
-        for i in (1..height.len()).rev() {
+        for i in (1..len).rev() {
             max = std::cmp::max(max, height[i]);
-            right[i-1] = max;
+            right_wall[i-1] = max;
         }
 
-        let mut x;
-        max = 0;
+        let mut wall;
 
-        for i in 0..height.len() {
-            x = std::cmp::min(left[i], right[i]);
-            max += std::cmp::max(0, x - height[i]);
+        for i in 0..len {
+            wall = std::cmp::min(left_wall[i], right_wall[i]);
+            water += std::cmp::max(0, wall - height[i]);
         }
 
-        max
+        water
+    }
+}
+
+// Solution with o(1) space - with no lists for left and right just two markers l and r
+pub struct Solution2 {}
+
+impl Solution2 {
+    pub fn trap(height: Vec<i32>) -> i32 {
+        // track l and r markers and lmax and rmax values 
+        let len = height.len();
+        let (mut l, mut r) = (0, len-1);
+        let (mut lmax, mut rmax) = (height[0], height[len-1]);
+        let mut water = 0;
+
+        while l < r {
+            if lmax <= rmax {
+                l += 1; // increase as l moves to right
+                // lmax is less than rmax so don't need to take min this time
+                water += std::cmp::max(0, lmax - height[l]);
+                lmax = std::cmp::max(lmax, height[l]); // update lmax
+            } else {
+                r -= 1; // decrease as r moves to left
+                water += std::cmp::max(0, rmax - height[r]);
+                rmax = std::cmp::max(rmax, height[r]); // update rmax
+            }
+        }
+
+        water
     }
 }
 
@@ -76,8 +103,8 @@ pub mod tests {
     
     #[test]
     pub fn test_0042() {
-        assert_eq!(6, Solution::trap(vec![0,1,0,2,1,0,1,3,2,1,2,1]));
-        assert_eq!(9, Solution::trap(vec![4,2,0,3,2,5]));
+        assert_eq!(6, Solution2::trap(vec![0,1,0,2,1,0,1,3,2,1,2,1]));
+        assert_eq!(9, Solution2::trap(vec![4,2,0,3,2,5]));
     }
 }
 
