1bbf8557257ff8eaac37d937412af2c497c162aa
Bibek Pandey <bibekp@gmail.com>
2023-09-14 13:06:34 -0700

    Added Box ListNode to 2 add two numbers instead of relying on LinkedList type

--- rust/src/p0002_add_two_numbers.rs
+++ rust/src/p0002_add_two_numbers.rs
@@ -1,4 +1,3 @@
-use std::collections::LinkedList;
 
 /*
 https://leetcode.com/problems/add-two-numbers/
@@ -16,9 +15,54 @@ Output: 7 -> 0 -> 8
 Explanation: 342 + 465 = 807.
 */
 
+use std::collections::LinkedList;
+
+use crate::util::ListNode;
+
+
+// Solution uses Box ListNode
 pub struct Solution{}
 
 impl Solution {
+    pub fn add_two_numbers(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let mut n1 = &l1;
+        let mut n2 = &l2;
+
+        let mut acc_carry: u32 = 0;
+        let mut sum_digit: u32;
+        let mut temp_sum: u32 = 0;
+
+        let mut dummy_head = ListNode::new(-1);
+        let mut tail = &mut dummy_head;
+
+        while n1.is_some() || n2.is_some() {
+            if let Some(ref v1) = n1 {
+                temp_sum += v1.data as u32;
+                n1 = &n1.as_ref().unwrap().next
+            }
+
+            if let Some(ref v2) = n2 {
+                temp_sum += v2.data as u32;
+                n2 = &n2.as_ref().unwrap().next
+            }
+
+            sum_digit = (temp_sum + acc_carry) % 10; // store the sum digit in newly created node
+            acc_carry = (temp_sum + acc_carry) / 10; // track the carry to be used on next loop iteration
+
+            tail.as_mut().unwrap().next = ListNode::new(sum_digit as i32);  // create the list chain
+            tail = &mut tail.as_mut().unwrap().next; // update the tail ptr to point to last element in chain
+
+            temp_sum = 0; // important to reset
+        }
+
+        dummy_head.as_mut().unwrap().next.take() // return head of new list
+    }
+}
+
+// Solution uses LinkedList type from std collections
+pub struct Solution1{}
+
+impl Solution1 {
 
     pub fn add_two_numbers_list(list1: &LinkedList<&u32>, list2: &LinkedList<&u32>) -> LinkedList<u32> {
         let mut output: LinkedList<u32> = LinkedList::new();
@@ -56,7 +100,7 @@ impl Solution {
     pub fn add_two_numbers(list1: &Vec<u32>, list2: &Vec<u32>) -> Vec<u32> {
         let l1: LinkedList<_> = list1.iter().collect::<LinkedList<_>>();
         let l2: LinkedList<_> = list2.iter().collect::<LinkedList<_>>();
-        let output = Solution::add_two_numbers_list(&l1, &l2);
+        let output = Solution1::add_two_numbers_list(&l1, &l2);
         output.into_iter().collect::<Vec<_>>()
     }
 }
@@ -64,12 +108,21 @@ impl Solution {
 #[cfg(test)]
 mod tests {
     use super::*;
-    
+
     #[test]
     fn test_0002() {
-        assert_eq!(vec![7, 0, 8], Solution::add_two_numbers(&vec![2, 4, 3], &vec![5, 6, 4]));
-        assert_eq!(vec![7, 0, 8, 9], Solution::add_two_numbers(&vec![2, 4, 3], &vec![5, 6, 4, 9]));
-        assert_eq!(vec![7, 0, 8, 7], Solution::add_two_numbers(&vec![2, 4, 3, 7], &vec![5, 6, 4]));
+        // Solution1
+        assert_eq!(vec![7, 0, 8], Solution1::add_two_numbers(&vec![2, 4, 3], &vec![5, 6, 4]));
+        assert_eq!(vec![7, 0, 8, 9], Solution1::add_two_numbers(&vec![2, 4, 3], &vec![5, 6, 4, 9]));
+        assert_eq!(vec![7, 0, 8, 7], Solution1::add_two_numbers(&vec![2, 4, 3, 7], &vec![5, 6, 4]));
+
+        // Solution
+        assert_eq!(ListNode::to_list(&[7, 0, 8]),
+                   Solution::add_two_numbers(ListNode::to_list(&[2, 4, 3]), ListNode::to_list(&[5, 6, 4])));
+        assert_eq!(ListNode::to_list(&[7, 0, 8, 9]),
+                   Solution::add_two_numbers(ListNode::to_list(&[2, 4, 3]), ListNode::to_list(&[5, 6, 4, 9])));
+        assert_eq!(ListNode::to_list(&[7, 0, 8, 7]),
+                   Solution::add_two_numbers(ListNode::to_list(&[2, 4, 3, 7]), ListNode::to_list(&[5, 6, 4])));
     }
 }
 
