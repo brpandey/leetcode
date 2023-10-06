bf7c7e1252d2be748cb7ac1eb18713a880abad11
Bibek Pandey <bibekp@gmail.com>
2023-10-06 13:50:30 -0700

    Added a dfs backtracking solution to 17 letter combs of a phone number

--- rust/src/p0017_letter_comb_phone.rs
+++ rust/src/p0017_letter_comb_phone.rs
@@ -1,15 +1,55 @@
 use std::collections::HashMap;
 use std::collections::VecDeque;
 
-pub struct Solution {}
+
+
+
+// DFS Solution
+use std::iter::FromIterator;
+
+pub struct Solution0 {}
+
+impl Solution0 {
+    pub fn letter_combinations(digits: String) -> Vec<String> {
+        let mut result = vec![];
+        let tuple_list: [(u8, &str); 10] = [(0, ""), (1, ""), (2, "abc"), (3, "def"),
+                                     (4, "ghi"), (5, "jkl"), (6, "mno"), (7, "pqrs"), (8, "tuv"), (9, "wxyz")];
+
+        let mapping: HashMap<u8, &str> = HashMap::from_iter(tuple_list);
+        let mut current = vec![];
+
+        if digits.len() > 0 {
+            Self::dfs(0, &mut current, &mapping, digits.as_bytes(), &mut result);
+        }
+
+        result
+    }
+
+    pub fn dfs(i: usize, cur: &mut Vec<u8>, mapping: &HashMap<u8, &str>, digits: &[u8], res: &mut Vec<String>) {
+        // Base case
+        // 'ad' or 'ae'    '23'
+        if cur.len() == digits.len() {
+            res.push(String::from_utf8(cur.clone()).unwrap());
+            return
+        }
+
+        // e.g 2 => "abc"
+        let letters = mapping.get(&(digits[i] - b'0')).unwrap();
+
+        // e.g. "abc"
+        for b in letters.bytes() {
+            cur.push(b);
+            Self::dfs(i+1, cur, &mapping, digits, res);
+            cur.pop();
+        }
+    }
+}
 
 /*
  * ""abc
  *
  * pop a, code is def, hence => ad, ae, af
  *
-
-
  * Note A
  * Create the auxiliary queue before we iterate through main queue
  * This allows us to have remove from the main queue (1 mutable reference)
@@ -18,9 +58,12 @@ pub struct Solution {}
  * Pop off from main and push onto main - or 2 mutable references aka not allowed)
  */
 
+// BFS Solution 
+
+pub struct Solution {}
 
 impl Solution {
-    pub fn run(digits: &str) -> Vec<String> {
+    pub fn letter_combinations(digits: &str) -> Vec<String> {
         let at: [(u32, &str); 10] = [(0, ""), (1, ""), (2, "abc"), (3, "def"),
                                      (4, "ghi"), (5, "jkl"), (6, "mno"), (7, "pqrs"), (8, "tuv"), (9, "wxyz")];
         let mapping: HashMap<u32, &str> = at.iter().cloned().collect();
@@ -70,6 +113,8 @@ mod tests {
     #[test]
     fn test_0017(){
         let check: Vec<String> = vec!["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].iter().map(|x| x.to_string()).collect();
-        assert_eq!(check, Solution::run("23"));
+        assert_eq!(check.clone(), Solution::letter_combinations("23"));
+
+        assert_eq!(check, Solution0::letter_combinations("23".to_string()));
     }
 }
